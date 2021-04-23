dab721216b4acce15db3aaff1e2654982c3d017a
Javiistacks <javiann.marrero@icloud.com>
2021-04-22 23:53:26 -0500

    added employes  and values to seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -23,3 +23,20 @@ VALUES
   ('Alumni',              0,      8),
   ('Ghosts',              0,      9);
 
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+  ('No',              'Manager',      1,  1),
+  ('Micheal',         'Jordan',       2,  1),
+  ('Kobe',            'Bryant',       3,  2),
+  ('Charles',         'Barkely',      4,  2),
+  ('Shaq',            'ONeil',        4,  2),
+  ('Jason',           'Kidd',         4,  2),
+  ('Allen',           'Iverson',      5,  2),
+  ('Lebron',          'James',        6,  2),
+  ('Dwayne',          'Wade',         7,  3),
+  ('Scottie',         'Pippen',       8,  3),
+  ('Chris',           'Paul',         8,  5),
+  ('Vanessa',         'Williams',     8,  6),
+  ('Candace',         'Parker',       8,  4),
+  ('Jasmine',         'Price',        10, 1),
+  ('Stephanie',       'Lopez',        9,  1);
