f9b12090228bc0dca71066040e4b51258a777662
Javiistacks <javiann.marrero@icloud.com>
2021-04-22 23:49:45 -0500

    added departments and values

---
+++ db/seed.sql
@@ -0,0 +1,11 @@
+INSERT INTO departments (dept_name)
+VALUES
+  ('None'),
+  ('President'),
+  ('Executive'),
+  ('Education'),
+  ('Support'),
+  ('Scholarship'),
+  ('Pupil'),
+  ('Alumni'),
+  ('Deceased');
