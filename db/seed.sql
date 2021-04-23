f5fbb5b3ce443b5981b1d0b333912be5c15b3458
Javiistacks <javiann.marrero@icloud.com>
2021-04-22 23:51:44 -0500

    added roles and values

--- db/seed.sql
+++ db/seed.sql
@@ -9,3 +9,17 @@ VALUES
   ('Pupil'),
   ('Alumni'),
   ('Deceased');
+
+INSERT INTO roles (title, salary, dept_id)
+VALUES
+  ('None',                0,      1),
+  ('Headmaster',          200000, 2),
+  ('Deputy',              150000, 3),
+  ('Head of House',       120000, 3),
+  ('Professor',           100000, 4),
+  ('Support Staff',       50000,  5),
+  ('Prefect',             10000,  6),
+  ('Student',             0,      7),
+  ('Alumni',              0,      8),
+  ('Ghosts',              0,      9);
+
